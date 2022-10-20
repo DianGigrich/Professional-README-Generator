f0583f846bfac694bed5f501daf97a453cdae6c5
Dian Sherman <slayer_barrett_@hotmail.com>
2022-10-19 19:46:16 -0700

    fixed fetch link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,53 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const url = axios.get("https://api.github.com/licenses")
 const fs = require("fs")
 const answers = require("answers.js")
-LicenseChoice = answers.license.value
+const LicenseChoice = answers.license.value
 Console.log(LicenseChoice)
+
+const url = ("https://api.github.com/licenses/" + LicenseChoice + "?per_page=1")
+
+
 
 function renderLicenseBadge(license) {
   if (license === "") {
     return ""
   } else {
-    src = "https://img.shields.io/badge/License-" + (LicenseChoice) + "-green"
-    console.log(renderLicenseBadge)
-  }
+    return ("https://img.shields.io/badge/License-" + (LicenseChoice) + "-green"}
+
 }
+console.log(renderLicenseBadge)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(data) {
   if (license === "") {
     return ""
   } else {
-    url.url
-    console.log(renderLicenseLink)
-  }
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    
+    return [License URL](data.html_url)
+      }
 }
-
+console.log(renderLicenseLink)
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "") {
+  if (renderLicenseLink === "" || renderLicenseBadge === "") {
     return ""
   } else {
-    return (renderLicenseBadge(data.License.value),
-    renderLicenseLink(data.License.value),
-    console.log(renderLicenseSection))
+    return (renderLicenseBadge(),
+      renderLicenseLink(),
+      console.log(renderLicenseSection))
   }
 }
-let LicenseSection = renderLicenseSction(data)
+let LicenseSection = renderLicenseSection(data)
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.Title},
-  ## ${data.Description},
-  ## ${data.Installation},
-  ## ${data.Instructions},
-  ## ${data.Screenshot},
-  ## ${data.Credits},
-  ## ${data.License},
+function generateMarkdown(answers) {
+  return `# ${answers.Title},
+  ## ${answers.Description},
+  ## ${answers.Installation},
+  ## ${answers.Instructions},
+  ## ${answers.Screenshot},
+  ## ${answers.Credits},
+  ## ${answers.License},
+  ${LicenseSection}
 `;
 }
 
